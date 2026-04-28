cdfbfc5aa5204346bfd9b3c1e0e77fca1cea24bc
Mario Albornoz <mario.alb.andino@gmail.com>
2026-04-28 22:46:15 +0200

    added cpal dependency

--- src/effects/harmonizer.rs
+++ src/effects/harmonizer.rs
@@ -1,16 +1,22 @@
-use std::f32::consts::PI;
+use hound::WavSpec;
 
 use crate::audio::traits::Effect;
 
 pub struct Harmonizer{
-    pub vol: f64,
+    pub repeat_factor: usize,
+    pub audio_specs: WavSpec
 }
 
-impl Effect for Harmonizer{
+impl Effect for Harmonizer {
     fn apply(&self, samples: Vec<f64>) -> Vec<f64> {
-        samples
-            .into_iter()
-            .map(|s: f64| s + (PI as f64 * s as f64) * self.vol)
-            .collect()
+        let mut audio_buffer: Vec<f64> = vec![0.0; self.audio_specs.sample_rate as usize];
+        let mut transformed_samples:Vec<f64>  = Vec::with_capacity(samples.len() * self.repeat_factor);
+        let mut audio_buffer_index: usize = 0usize;
+
+        for sample in samples{
+            let shifted: f64 = audio_buffer[audio_buffer_index];
+            
+        }
+        return transformed_samples; 
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,29 @@ mod audio;
 mod effects;
 
 use audio::{io, pipeline::AudioPipeline};
-use effects::{reverb::Reverb, distortion::TubeDistortion};
-
-const  CHORD_AUDIO_PATH: &str = "audioFiles/clean-electric-guitar-sustained-positivity.wav";
+use effects::{distortion::TubeDistortion, harmonizer::Harmonizer, reverb::Reverb};
+const CHORD_AUDIO_PATH: &str = "audioFiles/clean-electric-guitar-sustained-positivity.wav";
 //const  SOLO_GUITAR_PATH: &str = "audioFiles/clean-electric-guitar-comfy-afternoon.wav";
 
 fn main() {
     let loaded_input = io::load_input(CHORD_AUDIO_PATH);
 
     let pipeline = AudioPipeline::new()
-                                    .add_effect(TubeDistortion{rate:9.0})
-                                    .add_effect(
-                                        Reverb{
-                                            decay: 0.45,
-                                            delay_rate_sec: 0.35,
-                                            sample_rate:loaded_input.specs.sample_rate as f64,
-                                            delay_sample_rate: 0.0,
-                                            lowpass_smoothing_factor:0.8,
-                                            buffer_count: 4,
-                                        });
-                                    //.add_effect(Harmonizer{vol : 0.5});
-                                
+        .add_effect(TubeDistortion { rate: 9.0 })
+        .add_effect(Reverb {
+            decay: 0.45,
+            delay_rate_sec: 0.35,
+            sample_rate: loaded_input.specs.sample_rate as f64,
+            delay_sample_rate: 0.0,
+            lowpass_smoothing_factor: 0.8,
+            buffer_count: 4,
+        })
+        .add_effect(Harmonizer {
+            repeat_factor: 2,
+            audio_specs: loaded_input.specs,
+        });
+
     let processed = pipeline.process(loaded_input.samples);
 
     io::save_file("audioFiles/output.wav", processed, loaded_input.specs);
-
 }
-
